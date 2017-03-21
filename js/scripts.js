d3b13e88bdbb91b385bb1df7974373faaa7f6f0f
Elan Baharie and LaTanya Donaldon <elan@>
2017-03-21 13:34:34 -0700

    add JS focusing on toggle, classes to html, and style to css

---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+
+  $("h2.javabox").click(function() {
+    $("div.javabox").toggle();
+  });
+
+  $("h2.operbox").click(function() {
+    $("div.operbox").toggle();
+  });
+
+  $("h2.varbox").click(function() {
+    $("div.varbox").toggle();
+  });
+
+  $("h2.varnamebox").click(function() {
+    $("div.varnamebox").toggle();
+  });
+
+  $("h2.functbox").click(function() {
+    $("div.functbox").toggle();
+  });
+
+  $("h2.methbox").click(function() {
+    $("div.methbox").toggle();
+  });
+
+  $("h2.argbox").click(function() {
+    $("div.argbox").toggle();
+  });
+
+  $("h2.parabox").click(function() {
+    $("div.parabox").toggle();
+  });
+
+  $("h2.returnbox").click(function() {
+    $("div.returnbox").toggle();
+  });
+
+  $("h2.chainbox").click(function() {
+    $("div.chainbox").toggle();
+  });
+
+  $("h2.stringbox").click(function() {
+    $("div.stringbox").toggle();
+  });
+
+  $("h2.boobox").click(function() {
+    $("div.boobox").toggle();
+  });
+
+  $("h2.undbox").click(function() {
+    $("div.undbox").toggle();
+  });
+
+  $("h2.NaNbox").click(function() {
+    $("div.NaNbox").toggle();
+  });
+
+  $("h2.escbox").click(function() {
+    $("div.escbox").toggle();
+  });
+
+  $("h2.alertbox").click(function() {
+    $("div.alertbox").toggle();
+  });
+
+  $("h2.commbox").click(function() {
+    $("div.commbox").toggle();
+  });
+
+  $("h2.jQbox").click(function() {
+    $("div.jQbox").toggle();
+  });
+
+  $("h2.attrbox").click(function() {
+    $("div.attrbox").toggle();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
